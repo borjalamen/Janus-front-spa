b32c4b869ca1ea30978035ef4ac50e570e395d53
ateres <aparejat17@gmail.com>
2025-10-23 12:23:28 +0200

    Estructura basica amb dependencies noves

--- node_modules/@angular/build/src/utils/worker-pool.js
+++ node_modules/@angular/build/src/utils/worker-pool.js
@@ -14,7 +14,7 @@ class WorkerPool extends piscina_1.Piscina {
     constructor(options) {
         const piscinaOptions = {
             minThreads: 1,
-            idleTimeout: 1000,
+            idleTimeout: 4_000,
             // Web containers do not support transferable objects with receiveOnMessagePort which
             // is used when the Atomics based wait loop is enable.
             atomics: process.versions.webcontainer ? 'disabled' : 'sync',
