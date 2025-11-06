0d616d7bf50e54503a3341464e23d98cec5cc533
JanoVGC <altisentbrescojan@alumnes.ilerna.com>
2025-11-06 22:33:10 +0100

    lenguaje solo falta comanda gitbash

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,19 +8,22 @@ import {MatButtonModule} from "@angular/material/button";
 import {MatIconModule} from "@angular/material/icon";
 import {MatDialog, MatDialogModule} from '@angular/material/dialog';
 import { LoginDialogComponent } from './login-dialog/login-dialog';
-
+import { TranslateModule } from '@ngx-translate/core'; 
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [RouterModule, MatSidenavModule, MatListModule, MatToolbarModule, MatButtonModule, MatIconModule, MatDialogModule,LoginDialogComponent]
+  imports: [RouterModule, MatSidenavModule, MatListModule, MatToolbarModule, MatButtonModule, MatIconModule, MatDialogModule, TranslateModule, LoginDialogComponent]
 })
 export class AppComponent {
   title = 'JanusHUB.v1';
 
-  constructor(public dialog: MatDialog,private router:Router) {}
+  constructor(public dialog: MatDialog,private router:Router,private translate: TranslateService) {
+     this.translate.setDefaultLang('es'); // idioma por defecto
+     this.translate.use('es'); // idioma activo
+  }
 
   openLoginDialog(): void {
     const dialogRef = this.dialog.open(LoginDialogComponent, {
