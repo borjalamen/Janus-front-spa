a9e3e78d1c3ed17a91ff955971eb07414137dc76
JanoVGC <altisentbrescojan@alumnes.ilerna.com>
2025-11-11 11:09:07 +0100

    procedimientos html y css

---
+++ src/app/procedimientos/procedimientos.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-procedimientos',
+  templateUrl: './procedimientos.component.html',
+  styleUrls: ['./procedimientos.component.css']
+})
+export class ProcedimientosComponent {
+  title = 'Procedimientos';
+}
