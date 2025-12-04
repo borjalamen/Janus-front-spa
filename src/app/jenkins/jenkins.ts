9f26c4ad4bafeef528c4571d3219f226266fd832
JanoVGC <altisentbrescojan@alumnes.ilerna.com>
2025-12-04 11:25:21 +0100

    pop-up jenkins

--- src/app/jenkins/jenkins.ts
+++ src/app/jenkins/jenkins.ts
@@ -1,18 +1,58 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-jenkins',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './jenkins.html',
-  styleUrl: './jenkins.css',
+  styleUrls: ['./jenkins.css'],
+
+
 })
 export class Jenkins {
+
+  
+  popupOpen = false;
+
+  newName = "";
+  newUrl = "";
+
+  customJenkins: { name: string; url: string }[] = [];
+
+constructor() {
+  const saved = localStorage.getItem("customJenkins");
+  this.customJenkins = saved ? JSON.parse(saved) : [];
+}
+
+openPopup() {
+  this.popupOpen = true;
+}
+
+closePopup() {
+  this.popupOpen = false;
+  this.newName = "";
+  this.newUrl = "";
+}
+
+savePopup() {
+  this.customJenkins.push({
+    name: this.newName,
+    url: this.newUrl
+  });
+
+  localStorage.setItem("customJenkins", JSON.stringify(this.customJenkins));
+
+  this.closePopup();
+}
+
   onJenkinsSelected(event: Event) {
-  const file = (event.target as HTMLInputElement).files?.[0];
-
-  if (!file) return;
-
-  console.log("Archivo Jenkins seleccionado:", file.name);
+  const input = event.target as HTMLInputElement;
+  if (input.files && input.files.length > 0) {
+    const file = input.files[0];
+    console.log("Fitxer seleccionat:", file);
+    
+  }
 }
 
 }
