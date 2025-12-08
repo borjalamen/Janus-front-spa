1fbd6265148d89bd2001f220f29223205a3e1678
JanoVGC <altisentbrescojan@alumnes.ilerna.com>
2025-12-08 19:14:10 +0100

    eliminar jenkins

--- src/app/jenkins/jenkins.ts
+++ src/app/jenkins/jenkins.ts
@@ -2,6 +2,12 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
+interface JenkinsItem {
+  name: string;
+  url: string;
+  selected: boolean;
+}
+
 @Component({
   selector: 'app-jenkins',
   imports: [FormsModule, CommonModule],
@@ -18,11 +24,20 @@ export class Jenkins {
   newName = "";
   newUrl = "";
 
-  customJenkins: { name: string; url: string }[] = [];
+   customJenkins: JenkinsItem[] = [];
+
+  deletePopupOpen = false;
+  jenkinsToDelete: { name: string; url: string } | null = null;
+  deleteMode = false;
 
 constructor() {
   const saved = localStorage.getItem("customJenkins");
-  this.customJenkins = saved ? JSON.parse(saved) : [];
+  const parsed: { name: string; url: string }[] = saved ? JSON.parse(saved) : [];
+  this.customJenkins = parsed.map(jk => ({
+    name: jk.name,
+    url: jk.url,
+    selected: false   
+  }));
 }
 
 openPopup() {
@@ -36,10 +51,12 @@ closePopup() {
 }
 
 savePopup() {
-  this.customJenkins.push({
+ const newJenkins: { name: string; url: string; selected: boolean } = {
     name: this.newName,
-    url: this.newUrl
-  });
+    url: this.newUrl,
+    selected: false
+  };
+  this.customJenkins.push(newJenkins);
 
   localStorage.setItem("customJenkins", JSON.stringify(this.customJenkins));
 
@@ -54,7 +71,54 @@ savePopup() {
     
   }
 }
+    toggleDeleteMode() {
+    this.deleteMode = !this.deleteMode;
+
+    if (!this.deleteMode) {
+      this.customJenkins.forEach(jk => jk.selected = false);
+    }
+  }
+
+  toggleSelect(jk: { selected?: boolean }) {
+    if (this.deleteMode) {
+      jk.selected = !jk.selected;
+    }
+  }
+  confirmDelete(jk?: JenkinsItem) {
+    this.jenkinsToDelete = jk || null;
+    this.deletePopupOpen = true;
+  }
+
+  cancelDelete() {
+    this.deletePopupOpen = false;
+    this.jenkinsToDelete = null;
+  }
+
+  deleteJenkins() {
+    if (this.jenkinsToDelete) {
+      this.customJenkins = this.customJenkins.filter(j => j !== this.jenkinsToDelete);
+        
+      } else {
+      this.customJenkins = this.customJenkins.filter(jk => !jk.selected);
+    }
+
+     localStorage.setItem("customJenkins", JSON.stringify(this.customJenkins));
+    this.deletePopupOpen = false;
+    this.jenkinsToDelete = null;
+    this.deleteMode = false;
 
 }
+  openJenkins(jk: JenkinsItem, event: Event) {
+     if (!this.deleteMode) {
+      window.open(jk.url, "_blank");
+    } else {
+      this.toggleSelect(jk);
+      event.preventDefault();
+
+
+}
+  }
+}
+
 
 
