5f0d4419125667ee10d2fda704b2ee824329b2a8
Raúl Gallego <rgallegoga@minsait.com>
2025-12-11 22:48:55 +0100

    Crear environments

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -3,7 +3,8 @@ export const environment = {
   production: false,
   name: 'dev',
   // API base URL for dev environment. Usando el host del route en service.yaml.
-  apiBaseUrl: 'https://janushub-dev.apps-crc.testing',
+  // Añadimos `/api` para llegar al endpoint real del backend (evita recibir el index.html).
+  apiBaseUrl: 'https://janushub-dev.apps-crc.testing/api',
   // Habilitar o deshabilitar flags específicas para dev
   featureFlagX: true
 };
