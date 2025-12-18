8e93265f759f16883a998b2b870431c78eeb8418
jan altisent <altisentbrescojan@alumnes.ilerna.com>
2025-12-18 15:40:57 +0100

    angular

--- .angular/cache/20.3.7/demo/vite/deps/@angular_platform-browser.js
+++ .angular/cache/20.3.7/demo/vite/deps/@angular_platform-browser.js
@@ -41,8 +41,8 @@ import {
 } from "./chunk-D5P5I7SG.js";
 import "./chunk-EITAFI4I.js";
 import "./chunk-L7TZZFVV.js";
-import "./chunk-HWYXSU2G.js";
 import "./chunk-JRFR6BLO.js";
+import "./chunk-HWYXSU2G.js";
 import "./chunk-MARUHEWW.js";
 import "./chunk-WDMUDEB6.js";
 export {
